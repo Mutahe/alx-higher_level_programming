e93242fa6c0bd965290e88ba66f61692182a015a
Mutahe <elizagatura@gmail.com>
2023-11-02 14:49:44 -0700

    correct

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,16 @@
-#!/usr/bin/pyhon3
-if __name__ = "__main__".
+#!/usr/bin/python3
+if __name__ == "__main__":
     import sys
 
-    arg = sys,arg
-    size = len(arg) - 1
+    args = sys.argv[1:]
+    size = len(args)
 
-    if size > 1
-    print("{} arguments",format(size))
-    for i in range (1, size +1)
-        print("{}: {}".format(i, arg[i]))
-
-    elif size == 0
-        print("{} argumens".format(size))
-
-    else
+    if size > 1:
         print("{} arguments".format(size))
-        print("{}:{}".format(size, arg[i]))
+        for i in range(size):
+            print("{}: {}".format(i, args[i]))
+    elif size == 1:
+        print("{} arguments".format(size))
+        print("1: {}".format(args[0]))
+    else:
+        print("No arguments")
