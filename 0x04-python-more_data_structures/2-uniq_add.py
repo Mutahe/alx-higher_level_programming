c4d4891d83035e3f92a0dd2d29ef1d27f456c571
Mutahe <elizagatura@gmail.com>
2023-11-08 11:06:05 -0800

    correct add uniques

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,6 +2,6 @@
 def uniq_add(my_list=[]):
     uniq_ints = set()
     for t in my_list:
-        if t == uniq_ints:
+        if t != uniq_ints:
             total = uniq_ints.add(t)
     return total
